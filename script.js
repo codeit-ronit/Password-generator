a5f69e64227dabc678a4545c02c2c5efea078f32
codeit-ronit <152790532+codeit-ronit@users.noreply.github.com>
2024-03-13 10:12:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,186 +1,110 @@
-const passwordDisplay = document.querySelector('.password-display');
-const passwordPlaceholder = document.querySelector('.password-placeholder');
-const passwordCopyButton = document.querySelector('.copy-btn');
-const passwordCopiedNotification = document.querySelector('.copied-text');
+let sliderLength = document.querySelector(".char-length-slider");
+let sliderCount = document.querySelector(".char-count");
+let passwordDisplay = document.querySelector(".password-placeholder");
+let upperCase = document.getElementById("uppercase");
+let lowerCase = document.getElementById("lowercase");
+let numbers = document.getElementById("numbers");
+let symbols = document.getElementById("symbols");
+const genBtn = document.querySelector(".generate-btn");
+const form = document.querySelector(".password-settings");
+const copyBtn = document.querySelector(".copy-btn");
 
-const passwordForm = document.querySelector('.password-settings');
-const lengthSlider = document.querySelector('.char-length-slider');
-const charCount = document.querySelector('.char-count');
-const checkBoxes = document.querySelectorAll('input[type=checkbox]');
-
-const strengthDescription = document.querySelector('.strength-rating-text');
-const strengthRatingBars = document.querySelectorAll('.bar');
-
-const CHARACTER_SETS = {
-  uppercase: ['ABCDEFGHIJKLMNOPQRSTUVWXYZ', 26],
-  lowercase: ['abcdefghijklmnopqrstuvwxyz', 26],
-  numbers: ['1234567890', 10],
-  symbols: ['!@#$%^&*()', 10],
-}
-
-let canCopy = false;
-
-const getSliderVal = () => {
-  charCount.textContent = lengthSlider.value;
-}
-
-const styleRangeSlider = () => {
-  const min = lengthSlider.min;
-  const max = lengthSlider.max;
-  const val = lengthSlider.value;
-
-  lengthSlider.style.backgroundSize = (val - min) * 100 / (max - min) + '% 100%';
-}
-
-const handleSliderInput = () => {
-  getSliderVal();
-  styleRangeSlider();
-}
-
-//------------------------------------------------------//
-//--------------------STRENGTH METER--------------------//
-//------------------------------------------------------//
-
-// Remove colors applied to the strength meter
-const resetBarStyles = () => {
-  strengthRatingBars.forEach(bar => {
-    bar.style.backgroundColor = 'transparent';
-    bar.style.borderColor = 'hsl(252, 11%, 91%)';
-  });
-}
-
-// Fill in specified meter bars with the provided color
-const styleBars = ([...barElements], color) => {
-  barElements.forEach(bar => {
-    bar.style.backgroundColor = color;
-    bar.style.borderColor = color;
-  });
-}
+let checkBoxArr = Array.from(document.querySelectorAll("input[type='checkbox']"));
 
 
-// Display text description of password strength and
-// fill in the appropriate meter bars
-const styleMeter = (rating) => {
-  const text = rating[0];
-  const numBars = rating[1];
-  const barsToFill = Array.from(strengthRatingBars).slice(0, numBars);
-  
-  resetBarStyles();
 
-  strengthDescription.textContent = text;
+// strength determining varibles 
+let stengthDisplay = document.querySelector(".strength-label");
+let bars = document.querySelectorAll(".bar");
 
-  switch(numBars) {
-    case 1:
-      return styleBars(barsToFill, 'hsl(0, 91%, 63%)');
-    case 2:
-      return styleBars(barsToFill, 'hsl(13, 95%, 66%)');
-    case 3:
-      return styleBars(barsToFill, 'hsl(42, 91%, 68%)');
-    case 4:
-      return styleBars(barsToFill, 'hsl(127, 100%, 82%');
-    default:
-      throw new Error('Invalid value for numBars');
-  }
-}
+// function to generate password
+genBtn.addEventListener("click",()=>{
+    passwordDisplay.textContent = generatePassword();
+    passwordDisplay.style.color = "hsl(0, 0%, 100%)";
+    passwordDisplay.style.opacity = 1;
+    strengthHandle();
+})
 
-//-----------------------------------------------------------//
-//--------------------PASSWORD GENERATION--------------------//
-//-----------------------------------------------------------//
 
-// Calculate password entropy to determine strength
-// Return an array containing
-// the password strength description to display and the number
-// of bars in the meter to be filled
-const calcStrength = (passwordLength, charPoolSize) => {
-  const strength = passwordLength * Math.log2(charPoolSize);
+function generatePassword(){
+    let upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let lowerChars = "abcdefghijklmnopqrstuvwxyz";
+    let allNumbers = "0123456789";
+    let allSymbols = "@!#$%&~^*";
 
-  if(strength < 25) {
-    return ['Too Weak!', 1];
-  } else if (strength >= 25 && strength < 50) {
-    return ['Weak', 2];
-  } else if (strength >= 50 && strength < 75) {
-    return ['Medium', 3];
-  } else {
-    return ['Strong', 4];
-  }
-}
+    let genPassword="";
+    let allChars="";
 
-const generatePassword = (e) => {
-  e.preventDefault();
-  try {
-    validateInput();
+    allChars += lowerCase.checked ? lowerChars : "";
+    allChars += upperCase.checked ? upperChars : "";
+    allChars += numbers.checked ? allNumbers : "";
+    allChars += symbols.checked ? allSymbols : "";
 
-    let generatedPassword = '';
-    let includedSets = [];
-    let charPool = 0;
-
-    checkBoxes.forEach(box => {
-      if(box.checked) {
-        includedSets.push(CHARACTER_SETS[box.value][0]);
-        charPool += CHARACTER_SETS[box.value][1];
-      }
-    });
-
-    if (includedSets) {
-      for(let i=0; i<lengthSlider.value; i++) {
-        const randSetIndex = Math.floor(Math.random() * includedSets.length);
-        const randSet = includedSets[randSetIndex];
-
-        const randCharIndex = Math.floor(Math.random() * randSet.length);
-        const randChar = randSet[randCharIndex];
-        
-        generatedPassword += randChar;
-      }
+    if(allChars=="" || allChars.length==0){
+        return genPassword;
     }
     
-    const strength = calcStrength(lengthSlider.value, charPool);
-    styleMeter(strength);
-
-    passwordDisplay.textContent = generatedPassword;
-    canCopy = true;
-  } catch(err) {
-    console.log(err);
-  }
+    let i=1;
+    while (i<=sliderLength.value){
+        genPassword += allChars.charAt(Math.floor(Math.random()*allChars.length));
+        i++;
+    }
+    return genPassword;
 }
 
 
-const validateInput = () => {
-  // At least one box is checked
-  if(Array.from(checkBoxes).every(box => box.checked === false)) {
-    throw new Error('Make sure to check at least one box');
-  }
+sliderLength.addEventListener('input',()=>{
+    sliderCount.textContent = sliderLength.value;
+    sliderLength.style.backgroundSize = (sliderLength.value/10)*100 - 5 + "% 100%";
+})
+function strengthHandle(){
+    let checkedArrLength = checkBoxArr.filter(item => {
+        return item.checked;
+    })
+    let barsToColor = 0;
+    let strengthToShow = "STRENGTH";
+    let barColor = "";
+    if (sliderLength.value < 8 || checkedArrLength.length < 3){
+        strengthToShow = "Weak";
+        barsToColor = 2
+        barColor = "red";
+    }else if (sliderLength.value < 8 && checkedArrLength.length >= 3){
+        strengthToShow = "Weak";
+        barsToColor = 2;
+        barColor = "red";
+    }else if (sliderLength.value < 8 && checkedArrLength.length < 3){
+        strengthToShow = "Weak";
+        barsToColor = 2;
+        barColor = "red";
+    }else if (sliderLength.value >= 8 && checkedArrLength.length === 3){
+        strengthToShow = "Medium";
+        barsToColor = 3;
+        barColor = "Yellow";
+    }else if (sliderLength.value >= 8 && checkedArrLength.length === 4){
+        strengthToShow = "Strong";
+        barsToColor = 4;
+        barColor = "hsl(127, 100%, 82%)";
+    }
+
+    for(let i = 0;i<barsToColor;i++){
+        bars[i].style.backgroundColor = barColor;
+    }
+    for(let i = barsToColor;i<checkBoxArr.length;i++){
+        bars[i].style.backgroundColor = "";
+    }
+    stengthDisplay.textContent = strengthToShow;
+    stengthDisplay.style.color = "white";
 }
 
-//-----------------------------------------------------//
-//--------------------COPY PASSWORD--------------------//
-//-----------------------------------------------------//
 
-const copyPassword = async () => {
-  if(!passwordDisplay.textContent || passwordCopiedNotification.textContent) return;
-  if(!canCopy) return;
+// form submission preventing default behavior
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+})
 
-  await navigator.clipboard.writeText(passwordDisplay.textContent);
-  passwordCopiedNotification.textContent = 'Copied';
-
-  // Fade out text after 1 second
-  setTimeout(() => {
-    passwordCopiedNotification.style.transition = 'all 1s';
-    passwordCopiedNotification.style.opacity = 0;
-
-    // Remove styles and text after fade out
-    setTimeout(() => {
-      passwordCopiedNotification.style.removeProperty('opacity');
-      passwordCopiedNotification.style.removeProperty('transition');
-      passwordCopiedNotification.textContent = '';
-    }, 1000);
-  }, 1000);
-  
-}
-
-// Initialize number of characters on page load specified 
-// by the range input
-charCount.textContent = lengthSlider.value;
-
-passwordCopyButton.addEventListener('click', copyPassword);
-lengthSlider.addEventListener('input', handleSliderInput);
-passwordForm.addEventListener('submit', generatePassword);
+// copying password to clipboard
+copyBtn.addEventListener("click",()=>{
+    if (passwordDisplay!="" || passwordDisplay>=1){
+        navigator.clipboard.writeText(passwordDisplay.textContent);
+    }
+})
